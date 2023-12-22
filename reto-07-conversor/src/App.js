6003735d17dcc99b7c2a72648e5c9db06f9c8618
luisnarv <115510650+luisnarv@users.noreply.github.com>
2023-12-22 08:33:47 -0500

    conversor

---
+++ reto-07-conversor/src/App.js
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [value, setValue] = useState(1);
+  const [mainMoney, setainMoney] = useState("EUR");
+  const [secundaryMoney, setSecundayMoney] = useState("USD");
+  const [convert, setConvert] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  //`https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+
+  useEffect(() => {
+    async function frankfurterApi() {
+      if (secundaryMoney === mainMoney)
+        return alert("You cannot convert a value with the same currency");
+      if (!mainMoney || !secundaryMoney) return;
+      try {
+        setIsLoading(true);
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${value}&from=${mainMoney}&to=${secundaryMoney}`
+        );
+        const Response = await res.json();
+        setConvert(Response.rates[secundaryMoney]);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error.message, "esto es un error");
+      }
+    }
+    frankfurterApi();
+  }, [mainMoney, secundaryMoney]);
+
+  return (
+    <div className="App">
+      <input
+        type="number"
+        name=""
+        value={value}
+        onChange={(e) => setValue((value) => Number(e.target.value))}
+        disabled={isLoading}
+      />
+      <select
+        disabled={isLoading}
+        value={mainMoney}
+        onChange={(e) => setainMoney(e.target.value)}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+
+      <select
+        disabled={isLoading}
+        value={secundaryMoney}
+        onChange={(e) => setSecundayMoney(e.target.value)}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+
+      <p>{convert ? convert + " " + secundaryMoney : "OUTPUT"}</p>
+      {isLoading ? <p>LOADING ...</p> : ""}
+    </div>
+  );
+}
+
+export default App;
---
+++ reto-07-conversor/src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+reportWebVitals();
