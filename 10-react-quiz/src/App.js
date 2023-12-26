8155cd2bb9265cf0d613bc6cf5158e5f660884e1
luisnarv <115510650+luisnarv@users.noreply.github.com>
2023-12-26 10:08:39 -0500

    React-Quiz

--- 10-react-quiz/src/App.js
+++ 10-react-quiz/src/App.js
@@ -1,9 +1,58 @@
-import DateCounter from "./components/DateCounter";
+//import DateCounter from "./components/DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Loader from "./components/Loader";
+import Error from "./components/Error";
+import StartScreen from "./components/StartScreen";
+
+const initialState = {
+  questions: [],
+
+  //"loading, error, ready, active, finished
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+
+    case "dataFailed":
+      return { ...state, status: "error" };
+
+    default:
+      throw new Error("Action unkonwn");
+  }
+}
 
 function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const numQuestions = questions.length;
+
+  useEffect(function () {
+    async function getData() {
+      try {
+        const getData = await fetch(`http://localhost:8001/questions`);
+        const data = await getData.json();
+        dispatch({ type: "dataReceived", payload: data });
+        console.log(data);
+      } catch (error) {
+        dispatch({ type: "dataFailed" });
+        console.error(error.message);
+      }
+    }
+    getData();
+  }, []);
+
   return (
     <div className="App">
-      <DateCounter />
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQuestions={numQuestions} />}
+      </Main>
     </div>
   );
 }
