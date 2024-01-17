b7032896df99f7de36e085abc0ab205943eab842
luisnarv <115510650+luisnarv@users.noreply.github.com>
2024-01-17 02:19:41 -0500

    createORder

--- 15-fast-react-pizza/src/App.jsx
+++ 15-fast-react-pizza/src/App.jsx
@@ -3,7 +3,9 @@ import Home from "./UI/Home";
 import Menu, { loader as menuLoader } from "./Features/menu/Menu";
 import Cart from "./Features/cart/Cart";
 import Order, { loader as orderLoader } from "./Features/order/Order";
-import CreateOrder from "./Features/order/CreateOrder";
+import CreateOrder, {
+  action as actionNewOrder,
+} from "./Features/order/CreateOrder";
 import AppLayout from "./UI/AppLayout";
 import Error from "./UI/Error";
 
@@ -19,7 +21,11 @@ const router = createBrowserRouter([
         errorElement: <Error />,
       },
       { path: "/cart", element: <Cart /> },
-      { path: "/order/new", element: <CreateOrder /> },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+        action: actionNewOrder,
+      },
       {
         path: "/order/:orderID",
         element: <Order />,
