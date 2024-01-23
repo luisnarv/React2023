26dc875dc26a44499da7c7d1d4661e0dbf85a49d
luisnarv <115510650+luisnarv@users.noreply.github.com>
2024-01-23 03:10:39 -0500

    16-the-wild-oasis-Start

---
+++ 16-the-wild-oasis/src/App.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const H1 = styled.h1`
+  font-size: 30px;
+`;
+
+export default function App() {
+  return (
+    <div>
+      <H1>Hello world</H1>
+    </div>
+  );
+}
