0d98eb5a90a93458072872df2ec555f33d91c1bb
luisnarv <115510650+luisnarv@users.noreply.github.com>
2024-01-26 00:58:26 -0500

    react-hot-toast

--- 16-the-wild-oasis/src/App.jsx
+++ 16-the-wild-oasis/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 import Dashboard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
 import Cabins from "./pages/Cabins";
@@ -40,6 +41,22 @@ export default function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: 12 }}
+        toastOptions={{
+          succes: { duration: 3000 },
+          error: { duration: 5000 },
+          style: {
+            fontSize: "1.5rem",
+            maxWidth: "500px",
+            padding: "16px 24px",
+            backgroundColor: "var(--color-gray-0)",
+            color: "var(--color-gray-700)",
+          },
+        }}
+      />
     </QueryClientProvider>
   );
 }
--- 16-the-wild-oasis/src/services/apiCabins.js
+++ 16-the-wild-oasis/src/services/apiCabins.js
@@ -9,3 +9,14 @@ export async function getCabins() {
   }
   return data;
 }
+
+export async function deleteCabin(id) {
+  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+
+  if (error) {
+    console.log(error);
+    throw new Error("Cabins could not be delete");
+  }
+
+  return data;
+}
