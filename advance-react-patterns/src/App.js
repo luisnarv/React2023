113f090f11b6105f959b70ac786071c49e47ebcb
luisnarv <115510650+luisnarv@users.noreply.github.com>
2024-01-29 09:38:33 -0500

    Patron-Componetns

--- advance-react-patterns/src/App.js
+++ advance-react-patterns/src/App.js
@@ -1,135 +1,164 @@
-import { useState } from "react";
-import { faker } from "@faker-js/faker";
-import withToggles from "./HOC";
-//import "./styles.css";
-
-const products = Array.from({ length: 20 }, () => {
-  return {
-    productName: faker.commerce.productName(),
-    description: faker.commerce.productDescription(),
-    price: faker.commerce.price(),
-  };
-});
-
-const companies = Array.from({ length: 15 }, () => {
-  return {
-    companyName: faker.company.name(),
-    phrase: faker.company.catchPhrase(),
-  };
-});
-
-function ProductItem({ product }) {
-  return (
-    <li className="product">
-      <p className="product-name">{product.productName}</p>
-      <p className="product-price">${product.price}</p>
-      <p className="product-description">{product.description}</p>
-    </li>
-  );
-}
-
-function CompanyItem({ company, defaultVisibility }) {
-  const [isVisible, setIsVisisble] = useState(defaultVisibility);
-
-  return (
-    <li
-      className="company"
-      onMouseEnter={() => setIsVisisble(true)}
-      onMouseLeave={() => setIsVisisble(false)}
-    >
-      <p className="company-name">{company.companyName}</p>
-      {isVisible && (
-        <p className="company-phrase">
-          <strong>About:</strong> {company.phrase}
-        </p>
-      )}
-    </li>
-  );
-}
-
-function List({ title, items, render }) {
-  const [isOpen, setIsOpen] = useState(true);
-  const [isCollapsed, setIsCollapsed] = useState(false);
-
-  const displayItems = isCollapsed ? items.slice(0, 3) : items;
-
-  function toggleOpen() {
-    setIsOpen((isOpen) => !isOpen);
-    setIsCollapsed(false);
-  }
-
-  return (
-    <div className="list-container">
-      <div className="heading">
-        <h2>{title}</h2>
-        <button onClick={toggleOpen}>
-          {isOpen ? <span>&or;</span> : <span>&and;</span>}
-        </button>
-      </div>
-      {isOpen && <ul className="list">{displayItems.map(render)}</ul>}
-
-      <button onClick={() => setIsCollapsed((isCollapsed) => !isCollapsed)}>
-        {isCollapsed ? `Show all ${items.length}` : "Show less"}
-      </button>
-    </div>
-  );
-}
-
+import Counter from "./Counter";
+//------------------*Patron de componentes*---------------------------------
 export default function App() {
-  const ProductsListWithToggles = withToggles(ProductList);
-  const CompanyListWithToggles = withToggles(CompanisList);
-
   return (
     <div>
-      <h1>Render Props Demo</h1>
+      <h1>Compound Component Pattern</h1>
+      {/* <Counter
+        iconIncrease="+"
+        iconDecrease="-"
+        label="My NOT so flexible counter"
+        hideLabel={false}
+        hideIncrease={false}
+        hideDecrease={false}
+      /> */}
+      <Counter>
+        <Counter.Label>Increase</Counter.Label>
+        <br />
+        <Counter.Increase icon={"➕"} />
 
-      {/* <div className="col-2">
-        <List
-          title="Products"
-          items={products}
-          render={(product) => (
-            <ProductItem key={product.productName} product={product} />
-          )}
-        />
-        <List
-          title="Companies"
-          items={companies}
-          render={(company) => (
-            <CompanyItem
-              key={company.companyName}
-              company={company}
-              defaultVisibility={false}
-            />
-          )}
-        /> 
-      </div>
-        */}
-      <div className="col-2">
-        <ProductList title="Produts HOC" items={products} />
-        <ProductsListWithToggles title="Produts HOC" items={products} />
-        <CompanyListWithToggles title="Company HOC" items={companies} />
-      </div>
+        <Counter.Count />
+
+        <Counter.Decrease icon={"➖"} />
+      </Counter>
     </div>
   );
 }
 
-// LATER: Let's say we got this component from a 3rd-party library, and can't change it. But we still want to add the 2 toggle functionalities to it
-function ProductList({ title, items }) {
-  return (
-    <ul className="list">
-      {items.map((product) => (
-        <ProductItem key={product.productName} product={product} />
-      ))}
-    </ul>
-  );
-}
-function CompanisList({ title, items }) {
-  console.log(items[0].companyName, "company");
-  return (
-    <ul className="list">
-      {items.map((company) => (
-        <CompanyItem key={company.companyName} company={company} />
-      ))}
-    </ul>
-  );
-}
+//----------------------------------------------------------------/
+
+// import { useState } from "react";
+// import { faker } from "@faker-js/faker";
+// import withToggles from "./HOC";
+// //import "./styles.css";
+
+// const products = Array.from({ length: 20 }, () => {
+//   return {
+//     productName: faker.commerce.productName(),
+//     description: faker.commerce.productDescription(),
+//     price: faker.commerce.price(),
+//   };
+// });
+
+// const companies = Array.from({ length: 15 }, () => {
+//   return {
+//     companyName: faker.company.name(),
+//     phrase: faker.company.catchPhrase(),
+//   };
+// });
+
+// function ProductItem({ product }) {
+//   return (
+//     <li className="product">
+//       <p className="product-name">{product.productName}</p>
+//       <p className="product-price">${product.price}</p>
+//       <p className="product-description">{product.description}</p>
+//     </li>
+//   );
+// }
+
+// function CompanyItem({ company, defaultVisibility }) {
+//   const [isVisible, setIsVisisble] = useState(defaultVisibility);
+
+//   return (
+//     <li
+//       className="company"
+//       onMouseEnter={() => setIsVisisble(true)}
+//       onMouseLeave={() => setIsVisisble(false)}
+//     >
+//       <p className="company-name">{company.companyName}</p>
+//       {isVisible && (
+//         <p className="company-phrase">
+//           <strong>About:</strong> {company.phrase}
+//         </p>
+//       )}
+//     </li>
+//   );
+// }
+
+// function List({ title, items, render }) {
+//   const [isOpen, setIsOpen] = useState(true);
+//   const [isCollapsed, setIsCollapsed] = useState(false);
+
+//   const displayItems = isCollapsed ? items.slice(0, 3) : items;
+
+//   function toggleOpen() {
+//     setIsOpen((isOpen) => !isOpen);
+//     setIsCollapsed(false);
+//   }
+
+//   return (
+//     <div className="list-container">
+//       <div className="heading">
+//         <h2>{title}</h2>
+//         <button onClick={toggleOpen}>
+//           {isOpen ? <span>&or;</span> : <span>&and;</span>}
+//         </button>
+//       </div>
+//       {isOpen && <ul className="list">{displayItems.map(render)}</ul>}
+
+//       <button onClick={() => setIsCollapsed((isCollapsed) => !isCollapsed)}>
+//         {isCollapsed ? `Show all ${items.length}` : "Show less"}
+//       </button>
+//     </div>
+//   );
+// }
+
+// export default function App() {
+//   const ProductsListWithToggles = withToggles(ProductList);
+//   const CompanyListWithToggles = withToggles(CompanisList);
+
+//   return (
+//     <div>
+//       <h1>Render Props Demo</h1>
+
+//       {/* <div className="col-2">
+//         <List
+//           title="Products"
+//           items={products}
+//           render={(product) => (
+//             <ProductItem key={product.productName} product={product} />
+//           )}
+//         />
+//         <List
+//           title="Companies"
+//           items={companies}
+//           render={(company) => (
+//             <CompanyItem
+//               key={company.companyName}
+//               company={company}
+//               defaultVisibility={false}
+//             />
+//           )}
+//         />
+//       </div>
+//         */}
+//       <div className="col-2">
+//         <ProductList title="Produts HOC" items={products} />
+//         <ProductsListWithToggles title="Produts HOC" items={products} />
+//         <CompanyListWithToggles title="Company HOC" items={companies} />
+//       </div>
+//     </div>
+//   );
+// }
+
+// // LATER: Let's say we got this component from a 3rd-party library, and can't change it. But we still want to add the 2 toggle functionalities to it
+// function ProductList({ title, items }) {
+//   return (
+//     <ul className="list">
+//       {items.map((product) => (
+//         <ProductItem key={product.productName} product={product} />
+//       ))}
+//     </ul>
+//   );
+// }
+// function CompanisList({ title, items }) {
+//   console.log(items[0].companyName, "company");
+//   return (
+//     <ul className="list">
+//       {items.map((company) => (
+//         <CompanyItem key={company.companyName} company={company} />
+//       ))}
+//     </ul>
+//   );
+// }
